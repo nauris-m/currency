2dbc9b046625401064134c22254664bc27cfc172
nauris-m <nauris.m@gmail.com>
2019-09-12 16:26:36 +0300

    cleanup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiEndpoint: 'https://openexchangerates.org/api/latest.json',
-  apiKey: 'cb7dfcfb093f48739207d550ff2a22b0'
+  apiKey: 'apikey'
 };
